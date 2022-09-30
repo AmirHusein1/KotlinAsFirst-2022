ca257a4cdb23fe1340b18943972b92c62ca06347
AmirHusein1 <amirkhuseyn1001@gmail.com>
2022-09-30 15:03:51 +0300

    fourth commit

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -241,78 +241,42 @@ fun decimalFromString(str: String, base: Int): Int = TODO()
  * 90 = XC, 100 = C, 400 = CD, 500 = D, 900 = CM, 1000 = M.
  * Например: 23 = XXIII, 44 = XLIV, 100 = C
  */
+val firstRomeList = listOf("C", "CD", "D", "CM")
+val secondRomeList = listOf("X", "XL", "L", "XC")
+val thirdRomeList = listOf("I", "IV", "V", "IX")
 fun roman(n: Int): String {
     val m = mutableListOf<String>()
     val l = n / 1000
     for (i in 1..l) {
         m.add("M")
     }
-    when (val p = n % 1000 / 100) {
-        in 1..3 -> {
-            for (i in 1..p) {
-                m.add("C")
+    fun cap(list: List<String>, num: Int) {
+        when (num) {
+            in 1..3 -> {
+                for (i in 1..num) {
+                    m.add(list[0])
+                }
             }
-        }
 
-        4 -> {
-            m.add("CD")
-        }
-
-        in 5..8 -> {
-            m.add("D")
-            for (i in 6..p) {
-                m.add("C")
+            4 -> {
+                m.add(list[1])
             }
-        }
 
-        9 -> {
-            m.add("CM")
+            in 5..8 -> {
+                m.add(list[2])
+                for (i in 6..num) {
+                    m.add(list[0])
+                }
+            }
+
+            9 -> {
+                m.add(list[3])
+            }
         }
     }
-    when (val o = n % 1000 % 100 / 10) {
-        in 1..3 -> {
-            for (i in 1..o) {
-                m.add("X")
-            }
-        }
-
-        4 -> {
-            m.add("XL")
-        }
-
-        in 5..8 -> {
-            m.add("L")
-            for (i in 6..o) {
-                m.add("X")
-            }
-        }
-
-        9 -> {
-            m.add("XC")
-        }
-    }
-    when (val r = n % 10) {
-        in 1..3 -> {
-            for (i in 1..r) {
-                m.add("I")
-            }
-        }
-
-        4 -> {
-            m.add("IV")
-        }
-
-        in 5..8 -> {
-            m.add("V")
-            for (i in 6..r) {
-                m.add("I")
-            }
-        }
-
-        9 -> {
-            m.add("IX")
-        }
-    }
+    cap(firstRomeList, n % 1000 / 100)
+    cap(secondRomeList, n % 1000 % 100 / 10)
+    cap(thirdRomeList, n % 10)
     return m.joinToString("")
 }
 
@@ -328,104 +292,94 @@ fun main() {
     println(x1x2)
 }
 
+val firstList = listOf("", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять")
+val secondList = listOf("", "одна", "две", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять")
+val teens = listOf(
+    "десять",
+    "одиннадцать",
+    "двенадцать",
+    "тринадцать",
+    "четырнадцать",
+    "пятнадцать",
+    "шестнадцать",
+    "семнадцать",
+    "восемндацать",
+    "девятнадцать"
+)
+val tens = listOf(
+    "",
+    "двадцать",
+    "тридцать",
+    "сорок",
+    "пятьдесят",
+    "шестьдесят",
+    "семьдесят",
+    "восемьдесят",
+    "девяносто"
+)
+val hundreds = listOf(
+    "",
+    "сто",
+    "двести",
+    "триста",
+    "четыреста",
+    "пятьсот",
+    "шестьсот",
+    "семьсот",
+    "восемьсот",
+    "девятьсот"
+)
+
 fun russian(n: Int): String {
-    val firstList = listOf("", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять")
-    val secondList = listOf("", "одна", "две", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять")
-    val teens = listOf(
-        "десять",
-        "одиннадцать",
-        "двенадцать",
-        "тринадцать",
-        "четырнадцать",
-        "пятнадцать",
-        "шестнадцать",
-        "семнадцать",
-        "восемндацать",
-        "девятнадцать"
-    )
-    val tens = listOf(
-        "",
-        "двадцать",
-        "тридцать",
-        "сорок",
-        "пятьдесят",
-        "шестьдесят",
-        "семьдесят",
-        "восемьдесят",
-        "девяносто"
-    )
-    val hundreds = listOf(
-        "",
-        "сто",
-        "двести",
-        "триста",
-        "четыреста",
-        "пятьсот",
-        "шестьсот",
-        "семьсот",
-        "восемьсот",
-        "девятьсот"
-    )
     val a = n / 1000
     val b = n % 1000
-    val aHundred = a / 100
-    val aTens = a / 10 % 10
-    val aUnits = a % 10
-    val aTeens = a % 100
-    val bHundred = b / 100
-    val bTens = b / 10 % 10
-    val bUnits = b % 10
-    val bTeens = b % 100
-    val aList = mutableListOf<String>()
-    if (a > 0) {
-        if (aHundred in 1..9) {
-            aList.add(hundreds[aHundred])
+    fun translate(
+        num: Int,
+        unList: List<String>,
+        tenList: List<String>,
+        hunList: List<String>,
+        teenList: List<String>,
+        thousand: Boolean
+    ): String {
+        val list = mutableListOf<String>()
+        val hundred = num / 100
+        val tens = num / 10 % 10
+        val units = num % 10
+        val teens = num % 100
+        if (hundred in 1..9) {
+            list.add(hunList[hundred])
         }
-        if (aTeens in 10..19) {
-            aList.add(" ")
-            aList.add(teens[aTeens % 10])
-            aList.add(" тысяч ")
+        if (teens in 10..19) {
+            list.add(teenList[teens % 10])
         } else {
-            if (aTens in 2..9) {
-                aList.add(" ")
-                aList.add(tens[aTens - 1])
+            if (tens in 2..9) {
+                list.add(tenList[tens - 1])
             }
-            if (aUnits in 1..9) {
-                aList.add(" ")
-                aList.add(secondList[aUnits])
+            if (units in 1..9) {
+                list.add(unList[units])
             }
-            when (aUnits) {
+        }
+        if (thousand) {
+            when (units) {
                 in 2..4 -> {
-                    aList.add(" тысячи ")
+                    list.add("тысячи")
                 }
 
                 1 -> {
-                    aList.add(" тысяча ")
+                    list.add("тысяча")
                 }
 
                 else -> {
-                    aList.add(" тысяч ")
+                    list.add("тысяч")
                 }
             }
         }
+        return list.joinToString(" ")
     }
-    val bList = mutableListOf<String>()
-    if (b > 0) {
-        if (bHundred in 1..9) {
-            bList.add(hundreds[bHundred])
-            bList.add(" ")
-        }
-        if (bTeens in 10..19) {
-            bList.add(teens[bTeens % 10])
-        } else {
-            if (bTens in 2..9) {
-                bList.add(tens[bTens - 1])
-                bList.add(" ")
-            }
-            if (bUnits in 1..9) {
-                bList.add(firstList[bUnits])
-            }
-        }
+    return if (a > 0) {
+        (translate(a, secondList, tens, hundreds, teens, true) + " " +
+                translate(b, firstList, tens, hundreds, teens, false)).trim()
+    } else {
+        translate(b, firstList, tens, hundreds, teens, false)
     }
-    return (aList.joinToString("").trimStart() + bList.joinToString("").trimEnd()).trim()
 }
